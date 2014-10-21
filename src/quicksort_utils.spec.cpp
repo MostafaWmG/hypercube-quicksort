26c987bef47662f8795bc98b71bb8e42041393ec
Connor Bode <connor.bode@gmail.com>
2014-10-21 18:49:39 -0400

    test utils using double

--- src/quicksort_utils.spec.cpp
+++ src/quicksort_utils.spec.cpp
@@ -22,3 +22,18 @@ BOOST_AUTO_TEST_CASE(choosePivotTest) {
     BOOST_ASSERT(pivot <= max);
   }
 }
+
+BOOST_AUTO_TEST_CASE(choosePivotWorksWithDoubleTest) {
+  QuicksortUtils utils;
+  double testArray[4];
+  testArray[0] = 2.2;
+  testArray[1] = 3.3;
+  testArray[2] = 4.4;
+  testArray[3] = 5.5;
+
+  for (int i = 0; i < 10; i++) {
+    double pivot = utils.choosePivot(testArray, 4);
+    BOOST_ASSERT(pivot >= 2.2);
+    BOOST_ASSERT(pivot <= 5.5);
+  }
+}
