9cf017b8e514f540a6e4fdfe1c5f87bd072f138f
Connor Bode <connor.bode@gmail.com>
2014-10-21 19:40:37 -0400

    method to split a list on a pivot

--- src/quicksort_utils.cpp
+++ src/quicksort_utils.cpp
@@ -13,7 +13,7 @@ public:
    * Chooses a pivot using the median-of-three
    * method
    *
-   * @param {generic} list The list that we are sorting.  Type must implement addition and division.
+   * @param {Type} list The list that we are sorting.  Type must implement addition and division.
    * @param {int} length The length of the list we are sorting
    */
   template <typename Type>
@@ -23,6 +23,27 @@ public:
     Type thirdSample = list[rand() % length];
     Type pivot = (firstSample + secondSample + thirdSample) / 3;
     return pivot;
+  }
+
+
+  /**
+   * Splits a list on a pivot
+   *
+   * @param {Type} list The list to split.
+   * @param {int} length The length of the list
+   * @param {Type} pivot The pivot to split on
+   */
+  template <typename Type>
+  void splitList (Type list[], int length, Type pivot, Type lowArray[], Type highArray[], int &lowArrayLength, int &highArrayLength) {
+    for (int i = 0; i < length; i++) {
+      if (list[i] < pivot) {
+        lowArray[lowArrayLength] = list[i];
+        lowArrayLength += 1;
+      } else {
+        highArray[highArrayLength] = list[i];
+        highArrayLength += 1;
+      }
+    }
   }
 
 };
