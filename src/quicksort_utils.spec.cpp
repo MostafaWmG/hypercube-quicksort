9bfd87a96faad9a8238918abd8a3eff26a63f1f9
Connor Bode <connor.bode@gmail.com>
2014-10-22 16:40:15 -0400

    randomized quicksort test

--- src/quicksort_utils.spec.cpp
+++ src/quicksort_utils.spec.cpp
@@ -76,25 +76,13 @@ BOOST_AUTO_TEST_CASE(concreteSplitTestFour) {
   BOOST_ASSERT(lowLen == 1);
 }
 
-BOOST_AUTO_TEST_CASE(randomSplitTest) {
+BOOST_AUTO_TEST_CASE(concreteSortTest) {
   QuicksortUtils utils;
-  int midIndex, lowLen, highLen, length = 100, pivot = 0, midVal;
-  int testList[length];
-  int *lowList, *highList;
-  for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < length; j++) {
-      testList[j] = rand() % 100;
-    }
-    utils.split(testList, length, pivot, midIndex, lowLen, highLen);
-    lowList = &testList[0];
-    highList = &testList[midIndex];
-    midVal = testList[midIndex];
-    for (int j = 0; j < lowLen; j++) {
-      BOOST_ASSERT(lowList[j] < midVal);
-    }
-    for (int j = 0; j < highLen; j++) {
-      BOOST_ASSERT(highList[j] >= midVal);
-    }
+  int length = 10;
+  int testList[10] = {0, 4, 6, 2, 1, 7, 9, 8, 3, 5};
+  utils.sort(testList, length);
+  for (int i = 0; i < length; i++) {
+    BOOST_ASSERT(testList[i] == i);
   }
 }
 
@@ -107,4 +95,22 @@ BOOST_AUTO_TEST_CASE(concreteSortTestTwo) {
   BOOST_ASSERT(testList[2] == 2);
   BOOST_ASSERT(testList[3] == 2);
   BOOST_ASSERT(testList[4] == 4);
+}
+
+BOOST_AUTO_TEST_CASE(randomSortTest) {
+  srand(time(NULL));
+  QuicksortUtils utils;
+  int length = 100, current;
+  int testList[length];
+  for (int i = 0; i < length; i++) {
+    for (int j = 0; j < length; j++) {
+      testList[j] = rand() % 100;
+    }
+    utils.sort(testList, length);
+    current = 0;
+    for (int j = 0; j < length; j++) {
+      BOOST_ASSERT(testList[j] >= current);
+      current = testList[j];
+    }
+  }
 }
